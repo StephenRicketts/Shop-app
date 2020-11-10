469bc7be1f4605047c58047e75d1dde8be29361e
StephenRicketts <stephen.ricketts12@gmail.com>
2020-11-10 16:45:31 -0500

    Creating and updating user products added

--- screen/user/UserProductsScreen.js
+++ screen/user/UserProductsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, Button } from "react-native";
+import { FlatList, Button, Alert } from "react-native";
 import ProductItem from "../../components/shop/ProductItem";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../../components/UI/HeaderButton";
@@ -13,6 +13,22 @@ const UserProductScreen = (props) => {
 
   const editProductHandler = (id) => {
     props.navigation.navigate("EditProduct", { productId: id });
+  };
+
+  const deleteHandler = (id) => {
+    Alert.alert("Are you sure", "Do you really want to delete this item?", [
+      {
+        text: "No",
+        style: "default",
+      },
+      {
+        text: "Yes",
+        style: "destructive",
+        onPress: () => {
+          dispatch(productsActions.deleteProduct(id));
+        },
+      },
+    ]);
   };
 
   return (
@@ -37,9 +53,7 @@ const UserProductScreen = (props) => {
           <Button
             color={Colors.primary}
             title="Delete"
-            onPress={() => {
-              dispatch(productsActions.deleteProduct(itemData.item.id));
-            }}
+            onPress={deleteHandler.bind(this, itemData.item.id)}
           />
         </ProductItem>
       )}
@@ -68,7 +82,7 @@ UserProductScreen.navigationOptions = (navData) => {
           title="Add"
           iconName="md-create"
           onPress={() => {
-            navData.navigation.toggleDrawer();
+            navData.navigation.navigate("EditProduct");
           }}
         />
       </HeaderButtons>
